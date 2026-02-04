320ed16cadeb00e4af858b8db0cd7ba86c64f560
Md Rayhan Hossain <hmrayhan07@gmail.com>
2026-02-04 12:40:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -55,3 +55,13 @@ function switchLanguage() {
 function toggleMenu() {
   document.getElementById("mobileMenu").classList.toggle("active");
 }
+
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 10) {
+    navbar.style.boxShadow = "0 6px 18px rgba(0,0,0,0.08)";
+  } else {
+    navbar.style.boxShadow = "none";
+  }
+});
